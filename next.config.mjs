523995a2fe29f647865bc7c2380740bd7f656dcd
Justfemi <ajibade.olufemi@yahoo.com>
2024-09-22 18:20:37 +0100

    feat(pwa): added pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
+import withPWA from 'next-pwa';
+
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  ...nextConfig,
+});
